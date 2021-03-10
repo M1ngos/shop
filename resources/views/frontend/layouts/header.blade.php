9318129b7655c306bb9c16509c36873a3d14ac24
M1ngos <mingoemanuel07@gmail.com>
2021-03-10 20:50:39 +0200

    Header positon fixed

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,47 +1,36 @@
-<header class="header shop">
-    <!-- Topbar -->
-    <div class="topbar">
+ <header class="header shop"> 
+    <!-- Header Inner -->
+    <div class="header-inner">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-12 col-12">
-                    <!-- Top Left -->
-                    <div class="top-left">
-                        <ul class="list-main">
-                            @php
-                                $settings=DB::table('settings')->get();
-                                
-                            @endphp
-                            <li><i class="ti-headphone-alt"></i>@foreach($settings as $data) {{$data->phone}} @endforeach</li>
-                            <li><i class="ti-email"></i> @foreach($settings as $data) {{$data->email}} @endforeach</li>
-                        </ul>
+            <div class="cat-nav-head">
+                <div class="row">
+                    <div class="col-lg-12 col-12">
+                        <div class="menu-area">
+                            <!-- Main Menu -->
+                            <nav class="navbar navbar-expand-lg">
+                                <div class="navbar-collapse">	
+                                    <div class="nav-inner">	
+                                        <ul class="nav main-menu menu navbar-nav">
+                                            <li class="{{Request::path()=='home' ? 'active' : ''}}"><a href="{{route('home')}}">Pagina inicial</a></li>
+                                      
+                                            <li class="@if(Request::path()=='product-grids'||Request::path()=='product-lists')  active  @endif"><a href="{{route('product-grids')}}">Produtos</a><span class="new">Novos</span></li>												
+                                                {{Helper::getHeaderCategory()}}
+                                            <li class="{{Request::path()=='blog' ? 'active' : ''}}"><a href="{{route('blog')}}">Blog</a></li>									
+                                            <li class="{{Request::path()=='contact' ? 'active' : ''}}"><a href="{{route('contact')}}">Entre em contato conosco</a></li>
+                                            <li class="{{Request::path()=='about-us' ? 'active' : ''}}"><a href="{{route('about-us')}}">Sobre nós</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                            <!--/ End Main Menu -->	
+                        </div>
                     </div>
-                    <!--/ End Top Left -->
-                </div>
-                <div class="col-lg-6 col-md-12 col-12">
-                    <!-- Top Right -->
-                    <div class="right-content">
-                        <ul class="list-main">
-                        <li><i class="ti-location-pin"></i> <a href="{{route('order.track')}}">Acompanhar Pedido</a></li>
-                            {{-- <li><i class="ti-alarm-clock"></i> <a href="#">Promoção do Dia</a></li> --}}
-                            @auth 
-                                @if(Auth::user()->role=='admin')
-                                    <li><i class="ti-user"></i> <a href="{{route('admin')}}"  target="_blank">Painel de controle</a></li>
-                                @else 
-                                    <li><i class="ti-user"></i> <a href="{{route('user')}}"  target="_blank">Painel</a></li>
-                                @endif
-                                <li><i class="ti-power-off"></i> <a href="{{route('user.logout')}}">Sair</a></li>
-
-                            @else
-                                <li><i class="ti-power-off"></i><a href="{{route('login.form')}}">Conecte-se /</a> <a href="{{route('register.form')}}">Registro</a></li>
-                            @endauth
-                        </ul>
-                    </div>
-                    <!-- End Top Right -->
                 </div>
             </div>
         </div>
     </div>
-    <!-- End Topbar -->
+    <!--/ End Header Inner -->
+    
     <div class="middle-inner">
         <div class="container">
             <div class="row">
@@ -177,35 +166,5 @@
             </div>
         </div>
     </div>
-    <!-- Header Inner -->
-    <div class="header-inner">
-        <div class="container">
-            <div class="cat-nav-head">
-                <div class="row">
-                    <div class="col-lg-12 col-12">
-                        <div class="menu-area">
-                            <!-- Main Menu -->
-                            <nav class="navbar navbar-expand-lg">
-                                <div class="navbar-collapse">	
-                                    <div class="nav-inner">	
-                                        <ul class="nav main-menu menu navbar-nav">
-                                            <li class="{{Request::path()=='home' ? 'active' : ''}}"><a href="{{route('home')}}">Pagina inicial</a></li>
-                                            <li class="{{Request::path()=='about-us' ? 'active' : ''}}"><a href="{{route('about-us')}}">Sobre nós</a></li>
-                                            <li class="@if(Request::path()=='product-grids'||Request::path()=='product-lists')  active  @endif"><a href="{{route('product-grids')}}">Produtos</a><span class="new">Novos</span></li>												
-                                                {{Helper::getHeaderCategory()}}
-                                            <li class="{{Request::path()=='blog' ? 'active' : ''}}"><a href="{{route('blog')}}">Blog</a></li>									
-                                               
-                                            <li class="{{Request::path()=='contact' ? 'active' : ''}}"><a href="{{route('contact')}}">Entre em contato conosco</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </nav>
-                            <!--/ End Main Menu -->	
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/ End Header Inner -->
+    
 </header>
